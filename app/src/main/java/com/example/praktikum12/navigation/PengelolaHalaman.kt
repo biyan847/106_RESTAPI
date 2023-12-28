f61fc9c4812f5e45cfd5d9413fa499b4d2b9971b
febyan <febyan.putra.ft21@mail.umy.ac.id>
2023-12-28 20:44:56 +0700

    pengelola halaman

---
+++ app/src/main/java/com/example/praktikum12/navigation/PengelolaHalaman.kt
@@ -0,0 +1,38 @@
+package com.example.praktikum12.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.praktikum12.ui.home.Screen.DestinasiEntry
+import com.example.praktikum12.ui.home.Screen.DestinasiHome
+import com.example.praktikum12.ui.home.Screen.EntryKontakScreen
+import com.example.praktikum12.ui.home.Screen.HomeScreen
+
+@Composable
+fun PengelolaHalaman(navController: NavHostController = rememberNavController()) {
+    NavHost(
+        navController = navController,
+        startDestination = DestinasiHome.route,
+        modifier = Modifier
+    ) {
+        composable(DestinasiHome.route) {
+            HomeScreen(
+                navigateToItemEntry = { navController.navigate(DestinasiEntry.route) },
+                onDetailClick = {})
+        }
+        composable(DestinasiEntry.route) {
+            EntryKontakScreen(
+                navigateBack = {
+                    navController.navigate(DestinasiHome.route) {
+                        popUpTo(
+                            DestinasiHome.route
+                        ) { inclusive = true }
+                    }
+                },
+            )
+        }
+    }
+}
